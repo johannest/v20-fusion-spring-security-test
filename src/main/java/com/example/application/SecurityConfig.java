5c2c4ba793ea2b0671efb18f91f16edc881c89a1
Johannes Tuikkala <johannes@vaadin.com>
2021-04-13 12:25:24 +0300

    initial commit

--- src/main/java/com/example/application/SecurityConfig.java
+++ src/main/java/com/example/application/SecurityConfig.java
@@ -3,15 +3,12 @@ package com.example.application;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -31,25 +28,33 @@ public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/about").permitAll();//.anyRequest().hasAnyAuthority("ADMIN","USER");
-        http.authorizeRequests().antMatchers("/connect/**").permitAll();
-        // Set default security policy that permits Vaadin internal requests and
-        // denies all other
-        super.configure(http);
-        // use a form based login
+        http.authorizeRequests().antMatchers("/about").permitAll();
 
-        http.formLogin().loginPage(LOGIN_URL).permitAll().loginProcessingUrl(LOGIN_PROCESSING_URL)
-                .failureUrl(LOGIN_FAILURE_URL)
-                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler());
-        http.logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+        super.configure(http);
+
+        // use a form based login
+        http.formLogin();
+
+//        http.formLogin().loginPage(LOGIN_URL).permitAll()
+//                .loginProcessingUrl(LOGIN_PROCESSING_URL)
+//                .failureUrl(LOGIN_FAILURE_URL)
+//                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler());
+//        http.logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
     }
 
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         super.configure(web);
-        web.ignoring().antMatchers("/images/**");
-        web.ignoring().antMatchers("/icons/**"); // Allow anonymous requests to icons/
+        web.ignoring().antMatchers(
+                "/images/**",
+                "/icons/**",
+                "/VAADIN/**",
+                "/manifest.webmanifest",
+                "/sw.js",
+                "/offline-page.html",
+                "/frontend/**"
+        );
     }
 
     @Override
@@ -59,6 +64,6 @@ public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
                 .withUser("user").password(passwordEncoder.encode("123")).roles("USER")
                 .and()
                 .withUser("admin").password(passwordEncoder.encode("123")).roles("ADMIN", "USER")
-                ;
+        ;
     }
 }
